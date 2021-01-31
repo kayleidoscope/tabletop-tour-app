ee41aee2d4fb2be5b236c6063a6a84be029fe55f
Kay Holten <holtenkay@gmail.com>
2021-01-31 14:32:36 -0800

    static landing and about pages complete

---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer () {
+    return (
+        <footer>
+            <p>Copyright Kay Holten 2021</p>
+        </footer>
+    )
+}
