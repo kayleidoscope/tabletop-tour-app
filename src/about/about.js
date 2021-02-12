f34b78adeef4e516e08e99c9d5b21d1b903fa6c8
Kay Holten <holtenkay@gmail.com>
2021-02-11 21:44:02 -0800

    gives credit to Board Game Atlas

--- src/about/about.js
+++ src/about/about.js
@@ -9,11 +9,15 @@ class About extends Component {
         return (
             <section className="about">
                 <h2>About</h2>
+                <h3>The Story</h3>
                 <p>Tabletop Tour is the brainchild of software engineer Kay Holten.</p>
                 <p>One day, she realized how tired she was of having to remember all of the cool games she'd had seen advertisements for or had played.</p>
                 <p>And, thus, Tabletop Tour was born!</p>
                 <p>The site name pays homage to the board game Traveller's Tour Through the United States,
                      which was the first board game published in the United States (in 1822).</p>
+                <h3>Acknowledgements</h3>
+                <p>Thank you to <a href="https://www.boardgameatlas.com/">Board Game Atlas</a> for their awesome API, which provided all of the tabletop game data you'll find on this site.
+                    Bless you and your amazing documentation.</p>
                 <p>Happy gaming!</p>
             </section>
         )
